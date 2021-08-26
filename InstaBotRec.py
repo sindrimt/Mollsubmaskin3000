c81fa66db558358d1085bd16a46c01d53ae66354
sindrimt <sindrimt@gmail.com>
2021-08-26 11:04:17 +0200

    First Commit :)

---
+++ InstaBotRec.py
@@ -0,0 +1,72 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+def login(browser):
+    browser.get("https://www.instagram.com/")
+    # Velger den første knappen på siden (Endre dette senere?)
+    acceptAll = browser.find_element_by_css_selector("button")
+    acceptAll.click()
+
+    time.sleep(2)
+
+    username = browser.find_element_by_css_selector("[name = 'username']")
+    password = browser.find_element_by_css_selector("[name = 'password']")
+    login = browser.find_element_by_css_selector("button")
+
+    username.send_keys("saftigmann123")
+    password.send_keys("test12345!")
+    login.click()
+
+    time.sleep(5)
+
+
+def Vist_Tag(browser, url):
+    sleep_time = 5
+    browser.get(url)
+    time.sleep(sleep_time)
+
+    pictures = browser.find_elements_by_css_selector("div[class = '_9AhH0']")
+
+    image_count = 0
+
+    for picture in pictures:
+        if image_count >= 3:
+            break
+
+        picture.click()
+        time.sleep(sleep_time)
+
+        heart = browser.find_element_by_css_selector("[aria-label = 'Like']")
+        heart.click()
+
+        time.sleep(sleep_time)
+
+        close = browser.find_element_by_css_selector("[aria-label='Close']")
+        close.click()
+
+        time.sleep(sleep_time)
+
+        image_count += 1
+
+
+def main():
+    browser = webdriver.Chrome()
+    login(browser)
+
+    tags = [
+        "programming",
+        "softwaredeveloper",
+        "programminglife",
+        "programmerslife",
+        "programmerlife",
+        "developerlife",
+        "programmers",
+    ]
+
+    for tag in tags:
+        Vist_Tag(browser, f"https://www.instagram.com/explore/tags/{tag}")
+
+
+main()
