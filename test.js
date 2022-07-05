9cd4a81973730eaf288679a510893572e5e3d42e
sindrimt <sindrimt@gmail.com>
2022-07-05 20:53:08 +0200

    More or less complete logic

--- test.js
+++ test.js
@@ -53,22 +53,23 @@ let chordArray = [
     "Gmaj",
 ];
 
+// Filterts out duplicates
 let uniqueArray = chordArray.filter(function (item, pos) {
     return chordArray.indexOf(item) == pos;
 });
 
-console.log(uniqueArray);
-
-//console.log(Key.majorKey("C").chords[3]);
-
-//console.log(Chord.get("Fmaj7#9#11"));
-console.log(Chord.extended("Cmin"));
-
+// Finds the subdominant chord of the key and make it minor
 let subdominant = Key.majorKey("G").chords[3][0] + "min";
-console.log(subdominant);
 
-if (chordArray.includes(subdominant)) {
-    console.log("MOLLSUBB!");
-} else {
-    console.log("Ikke mollsub :(");
+// A list of all the extended chords of the subdominant minor chord
+let extended = Chord.extended(subdominant);
+
+// Push the subdominant chord to the list of extended chords so it checks it as well
+extended.push(subdominant);
+
+// Loop through the extended chords and find the ones that are in the uniqueArray
+for (let i = 0; i < extended.length; i++) {
+    if (uniqueArray.includes(extended[i])) {
+        console.log("MOLLSUB");
+    }
 }
