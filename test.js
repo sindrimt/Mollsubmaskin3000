20051771149680eba57fe32c31e7775da65790b5
sindrimt <sindrimt@gmail.com>
2022-07-06 22:51:26 +0200

    e

--- test.js
+++ test.js
@@ -54,22 +54,23 @@ let chordArray = [
 ];
 
 // Filterts out duplicates
-let uniqueArray = chordArray.filter(function (item, pos) {
-    return chordArray.indexOf(item) == pos;
-});
+// let uniqueArray = chordArray.filter(function (item, pos) {
+//     return chordArray.indexOf(item) == pos;
+// });
 
 // Finds the subdominant chord of the key and make it minor
-let subdominant = Key.majorKey("G").chords[3][0] + "min";
+let subdominant = Key.majorKey("D").chords[3][0] + "min";
+console.log(subdominant);
 
 // A list of all the extended chords of the subdominant minor chord
-let extended = Chord.extended(subdominant);
+//let extended = Chord.extended(subdominant);
 
 // Push the subdominant chord to the list of extended chords so it checks it as well
-extended.push(subdominant);
+//extended.push(subdominant);
 
 // Loop through the extended chords and find the ones that are in the uniqueArray
-for (let i = 0; i < extended.length; i++) {
-    if (uniqueArray.includes(extended[i])) {
-        console.log("MOLLSUB");
-    }
-}
+// for (let i = 0; i < extended.length; i++) {
+//     if (uniqueArray.includes(extended[i])) {
+//         console.log("MOLLSUB");
+//     }
+// }
