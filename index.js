9c0c841e5dcd10ddac54fdf21d72a2c2196f1685
sindrimt <sindrimt@gmail.com>
2022-07-21 20:38:40 +0200

    discord bot integration works

--- index.js
+++ index.js
@@ -1,149 +1,36 @@
-import { Builder, By, Key, until } from "selenium-webdriver";
-import { loopThroughSongs } from "./utils/utils.js";
+import Discord from "discord.js";
+import "dotenv/config";
 
-import { Key as TonalKey } from "@tonaljs/tonal";
-import { ChordType } from "@tonaljs/tonal";
-import { Chord } from "@tonaljs/tonal";
+import { main } from "./mollsub.js";
 
-let errorRetries = 0;
+const client = new Discord.Client({ intents: 32767 });
+const prefix = process.env.PREFIX;
 
-let searchSongName = "hard to say dan fogleberg";
-
-const main = (searchTerm) => {
-    let driver = new Builder().forBrowser("chrome", "/Applications/Google Chrome Beta.app/Contents/MacOS/Google Chrome Beta").build();
-    let chosenSong;
-    let key;
-
-    driver.get(`https://chordify.net/search/${searchTerm}`).then(async () => {
-        await driver.sleep(1000);
-        return (
-            driver
-                // Find all search results
-                .findElements(By.className("apm19lh l1kpohop"))
-                .then(async (res) => {
-                    // Loop through search results until it finds a chordified song
-                    return loopThroughSongs(res, errorRetries);
-                })
-                // Get the song and its element
-                .then((res) => {
-                    // We split on youtube to get the title of the song
-                    chosenSong = res[0].split("youtube")[0];
-                    let chosenSongElement = res[1];
-
-                    return chosenSongElement.click();
-                })
-                .then(async (res) => {
-                    console.log("Before cookie");
-
-                    // Waits until the accpect cookies shows
-                    await driver.wait(until.elementLocated(By.className("fc-button fc-cta-consent fc-primary-button")), 1000);
-
-                    // Click accept cookies button
-                    let acceptCookies = await driver.findElement(By.className("fc-button fc-cta-consent fc-primary-button"));
-
-                    //TODO: make the error retry into a function in utils
-                    return acceptCookies.click().catch(() => {
-                        errorRetries++;
-
-                        // Call recursive function to find the next song
-                        return main(searchSongName);
-                    });
-                })
-                .then(async () => {
-                    console.log("Cookies clicked!");
-                    await driver.sleep(2000);
-
-                    // Click on chord view
-                    let chordView = await driver.findElements(By.className("tuxs61x b1g4bi0i b69i0zi b136ilgh"));
-
-                    return chordView[1].click();
-                })
-                .then(async () => {
-                    // Identify the key of the song
-                    await driver.sleep(1000);
-                    let songKey = await driver
-                        .findElement(By.xpath("//span[@class = 'tboi1b']//span[starts-with(@class, 'label-')]"))
-
-                        // If the song does not have chords, even though it says "CHORDIFY NOW", we skip it and choose the next song on the list
-                        .catch(() => {
-                            errorRetries++;
-
-                            // Call recursive function to find the next song
-                            return main(searchSongName);
-                        });
-
-                    return songKey.getAttribute("class");
-                })
-                .then((keyName) => {
-                    // Get the actual key of the song
-                    key = keyName.split("-")[1];
-                    console.log("Key:", key);
-
-                    if (key.includes("min")) {
-                        throw new Error("Key is minor");
-                    }
-                })
-                .then(async () => {
-                    console.log("Chordview clicked!");
-                    await driver.sleep(1000);
-
-                    // If it cant find the chords, we try again with next song
-                    let chordGrid = await driver.findElement(By.className("chords cvhfkdk barlength-4")).catch(() => {
-                        errorRetries++;
-
-                        // Call recursive function to find the next song
-                        return main(searchSongName);
-                    });
-                    let chordArray = [];
-
-                    let jobs = [];
-
-                    // Finds all the chords only for the grid elements marked as chords
-                    return chordGrid
-                        .findElements(By.xpath("//div[@class = 'chord']//span[starts-with(@class, 'chord-label')]"))
-                        .then((chord) => {
-                            for (let i = 0; i < chord.length; i++) {
-                                jobs.push(
-                                    chord[i].getAttribute("class").then((chordName) => {
-                                        chordArray.push(chordName.split("label-")[1]?.replace("_", ""));
-                                    })
-                                );
-                            }
-                            return Promise.all(jobs);
-                        })
-                        .then(() => {
-                            console.log("done");
-
-                            // Filterts out duplicates
-                            let uniqueArray = chordArray.filter(function (item, pos) {
-                                return chordArray.indexOf(item) == pos;
-                            });
-                            //console.log(uniqueArray);
-
-                            // Finds the subdominant chord of the key and make it minor
-                            let subdominant = TonalKey.majorKey(key[0]).chords[3][0] + "min";
-                            //console.log("Subdominant:", subdominant);
-
-                            // A list of all the extended chords of the subdominant minor chord
-                            let extended = Chord.extended(subdominant);
-
-                            // Push the subdominant chord to the list of extended chords so it checks it as well
-                            extended.push(subdominant);
-
-                            // Loop through the extended chords and find the ones that are in the uniqueArray
-                            for (let i = 0; i < extended.length; i++) {
-                                if (uniqueArray.includes(extended[i])) {
-                                    console.log(chosenSong + "Is MOLLSUB!!");
-                                }
-                            }
-                        });
-                })
-                .catch((err) => {
-                    console.log(err);
-                    driver.quit();
-                })
-        );
+client.on("ready", (message) => {
+    console.log("serving");
+    client.channels.fetch("880738403040239678").then((channel) => {
+        //channel.send("test");
     });
-};
+});
 
-main(searchSongName);
+client.on("messageCreate", (message) => {
+    // If message starts with prefix, we do the following under
+    if (message.content.startsWith(prefix)) {
+        let userMessage = message.content.split(prefix)[1].trim();
+        console.log(userMessage);
+
+        message.channel.send(`Checking if "${userMessage}" has mollsub . . .`);
+
+        return main(userMessage)
+            .then((result) => {
+                console.log(result);
+                message.channel.send(`:partying_face: ${userMessage} is ${result} :partying_face:`);
+            })
+            .catch((err) => {
+                console.log(err);
+                message.channel.send(`${userMessage} is not mollsub :(`);
+            });
+    }
+});
+
+client.login(process.env.SECRET_TOKEN);
